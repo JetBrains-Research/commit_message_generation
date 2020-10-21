8d8a738fa8ac4282926aed674420bca9eda3108c
saridormi <saridormi123@gmail.com>
2020-10-21 14:15:12 +0300

    Print extra info

--- models/train_utils.py
+++ models/train_utils.py
@@ -90,6 +90,9 @@ def greedy_decode(model, batch, tokenizer: RobertaTokenizer, max_len=100):
     eos_index = tokenizer.eos_token_id
 
     with torch.no_grad():
+        print("Greedy decode")
+        print("input_ids", batch['input_ids'])
+        print("attention_mask", batch['attention_mask'])
         encoder_output, encoder_final = model.encode(batch['input_ids'], batch['attention_mask'])
         prev_y = torch.ones(1, 1).fill_(sos_index).type_as(batch['input_ids'])
         trg_mask = torch.ones_like(prev_y)
@@ -127,7 +130,6 @@ def greedy_decode(model, batch, tokenizer: RobertaTokenizer, max_len=100):
 def print_examples(example_iter: DataLoader, model: EncoderDecoder, tokenizer: RobertaTokenizer,
                    n=2, max_len=100) -> None:
     """Prints N examples. Assumes batch size of 1."""
-    model.eval()
     count = 0
     print()
 
