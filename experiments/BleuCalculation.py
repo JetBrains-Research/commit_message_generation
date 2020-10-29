d4540d7ec5ab3ba4f33168be784342926910e0dc
saridormi <saridormi123@gmail.com>
2020-10-29 21:14:01 +0300

    Refactor preprocessing of targets and predictions into separate methods

--- experiments/BleuCalculation.py
+++ experiments/BleuCalculation.py
@@ -1,10 +1,8 @@
 import subprocess
 import tempfile
 from typing import List, Tuple
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 from Config import Config
-
-# TODO: test BleuCalculation
 
 
 def isfloat(value):
@@ -20,12 +18,8 @@ class BleuCalculation:
         super().__init__()
         self.config = config
 
-    def get_bleu_script_output(self, predictions, dataset: dict) -> Tuple[str, str]:
-        top_1_predictions = ['' if len(prediction) == 0 else ' '.join([str(i) for i in prediction[0]]) for prediction in predictions]
-        pred_str = '\n'.join(top_1_predictions)
+    def get_bleu_script_output(self, pred_str, trg_str: str) -> Tuple[str, str]:
         print("BLEU top 1 predictions", pred_str)
-        targets = dataset['target']['input_ids'].tolist()
-        trg_str = '\n'.join([' '.join([str(i) for i in lst]) for lst in targets])
         print("BLEU targets", trg_str)
         with tempfile.NamedTemporaryFile(mode='w') as file_with_targets:
             file_with_targets.write(trg_str + '\n')
@@ -37,16 +31,16 @@ class BleuCalculation:
 
     def conduct(self, predictions: List[List[str]], dataset, dataset_label: str) -> None:
         print(f'Start conducting BLEU calculation experiment for {dataset_label}...')
-        data_iterator = DataLoader(dataset, batch_size=len(dataset))  # load whole dataset in one batch
-        for batch in data_iterator:
-            result = self.get_bleu_script_output(predictions, batch)
+        trg_str = self.preprocess_dataset_for_bleu(dataset)
+        pred_str = self.preprocess_predictions_for_bleu(predictions)
+        result = self.get_bleu_script_output(pred_str, trg_str)
         print(result[0])
         print(f'Errors: {result[1]}')
 
-    def get_bleu_score(self, predictions: List[List[List[str]]], dataset: dict) -> float:
-        data_iterator = DataLoader(dataset, batch_size=len(dataset))
-        for batch in data_iterator:
-            result = self.get_bleu_script_output(predictions, batch)
+    def get_bleu_score(self, predictions: List[List[List[str]]], dataset: Dataset) -> float:
+        trg_str = self.preprocess_dataset_for_bleu(dataset)
+        pred_str = self.preprocess_predictions_for_bleu(predictions)
+        result = self.get_bleu_script_output(pred_str, trg_str)
         print(result[0])
         words = result[0].split()
         if len(words) > 2 and len(words[2]) > 0 and isfloat(words[2][:-1]):
@@ -56,3 +50,25 @@ class BleuCalculation:
             print('Warning: something wrong with bleu score')
             print(f'Errors: {result[1]}')
             return 0
+
+    @staticmethod
+    def preprocess_dataset_for_bleu(dataset: Dataset, pad_token_id=1) -> str:
+        data_iterator = DataLoader(dataset, batch_size=len(dataset))  # load whole dataset in one batch
+        for batch in data_iterator:
+            targets = batch['target']['input_ids'].tolist()
+            # remove trailing pad tokens ids
+            for i, el in enumerate(targets):
+                try:
+                    ind = el.index(pad_token_id)
+                except ValueError:  # no padding
+                    ind = len(el)
+                targets[i] = el[:ind]
+            # separate elements in each row with spaces
+            # separate rows with newline \n
+            return '\n'.join([' '.join([str(i) for i in lst]) for lst in targets])
+
+    @staticmethod
+    def preprocess_predictions_for_bleu(predictions: List[List[List[str]]]) -> str:
+        top_1_predictions = ['' if len(prediction) == 0 else ' '.join([str(i) for i in prediction[0]]) for prediction in
+                             predictions]
+        return '\n'.join(top_1_predictions)
