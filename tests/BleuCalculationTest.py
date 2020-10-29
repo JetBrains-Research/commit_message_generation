7cc6d924dcfc204e94d3742bc9b0f1ca45ee4593
saridormi <saridormi123@gmail.com>
2020-10-29 11:23:39 +0300

    Update BleuCalculationTest

--- tests/BleuCalculationTest.py
+++ tests/BleuCalculationTest.py
@@ -1,7 +1,7 @@
 import unittest
-
+from dataset_utils.CommitMessageGenerationDataset import CommitMessageGenerationDataset
 from experiments.BleuCalculation import BleuCalculation
-
+import torch
 
 class BleuChecking(unittest.TestCase):
     CONFIG = {'BLEU_PERL_SCRIPT_PATH': './experiments/multi-bleu.perl'}
@@ -10,18 +10,16 @@ class BleuChecking(unittest.TestCase):
     def test_same_predicted_as_target(self):
         predictions = [
             [
-                'Fix android_media_AudioSystem_getMasterMute return type .'.split(),
-                'Not fix android_media_AudioSystem_getMasterMute return type .'.split()
+                [1, 2, 3, 4],
+                [0, 1, 2, 3, 4]
             ],
             [
-                'Fix typo'.split()
+                [1, 5, 600, 600]
             ]
         ]
-        dataset = {'target': {
-                   'input_ids': [
-                       'Fix android_media_AudioSystem_getMasterMute return type .'.split(),
-                       'Fix typo'.split()
-                   ]}}
+        dataset = CommitMessageGenerationDataset(src_encodings={}, trg_encodings={'input_ids': torch.tensor([
+                       [1, 2, 3, 4],
+                       [1, 5, 600, 600]])})
         BleuChecking.BLEU_CALCULATION_EXPERIMENT.conduct(predictions, dataset, 'test')
 
     def test_bleu_zero(self):
@@ -34,84 +32,68 @@ class BleuChecking(unittest.TestCase):
                 'Totally incorrect guess .'.split()
             ]
         ]
-        dataset = {'target': {
-                   'input_ids': [
-                         'Fix android_media_AudioSystem_getMasterMute return type .'.split(),
-                         'Fix typo'.split()
-        ]}}
+        dataset = CommitMessageGenerationDataset(src_encodings={}, trg_encodings={'input_ids': torch.tensor([
+            [1, 2, 3, 4],
+            [1, 5, 600, 600]])})
         BleuChecking.BLEU_CALCULATION_EXPERIMENT.conduct(predictions, dataset, 'test')
 
     def test_only_one_example(self):
         predictions = [
-            [
-                'Fix android_media_AudioSystem_getMasterMute return type .'.split(),
-                'Not fix android_media_AudioSystem_getMasterMute return type .'.split()
-            ]
+                [1, 2, 3, 4],
+                [0, 1, 2, 3, 4]
         ]
-        dataset = {'target': {
-                   'input_ids': [
-            'Fix android_media_AudioSystem_getMasterMute return type .'.split()
-        ]}}
+        dataset = CommitMessageGenerationDataset(src_encodings={}, trg_encodings={'input_ids': torch.tensor([
+            [1, 2, 3, 4]])})
         BleuChecking.BLEU_CALCULATION_EXPERIMENT.conduct(predictions, dataset, 'test')
 
     def test_no_predictions(self):
         predictions = [
             [], []
         ]
-        dataset = {'target': {
-                   'input_ids': [
-            'Fix android_media_AudioSystem_getMasterMute return type .'.split(),
-            'Fix typo'.split()
-        ]}}
+        dataset = CommitMessageGenerationDataset(src_encodings={}, trg_encodings={'input_ids': torch.tensor([
+            [1, 2, 3, 4],
+            [1, 5, 600, 600]])})
         BleuChecking.BLEU_CALCULATION_EXPERIMENT.conduct(predictions, dataset, 'test')
 
     def test_same_predicted_as_target_bleu_score(self):
         predictions = [
             [
-                'Fix android_media_AudioSystem_getMasterMute return type .'.split(),
-                'Not fix android_media_AudioSystem_getMasterMute return type .'.split()
+                [1, 2, 3, 4],
+                [0, 1, 2, 3, 4]
             ],
             [
-                'Fix typo'.split()
+                [1, 5, 600, 600]
             ]
         ]
-        dataset = {'target': {
-                   'input_ids': [
-                         'Fix android_media_AudioSystem_getMasterMute return type .'.split(),
-                         'Fix typo'.split()
-        ]}}
+        dataset = CommitMessageGenerationDataset(src_encodings={}, trg_encodings={'input_ids': torch.tensor([
+            [1, 2, 3, 4],
+            [1, 5, 600, 600]])})
         bleu_score = BleuChecking.BLEU_CALCULATION_EXPERIMENT.get_bleu_score(predictions, dataset)
         self.assertEqual(100.0, bleu_score)
 
     def test_bleu_zero_bleu_score(self):
         predictions = [
             [
-                'Completely incorrect guess .'.split(),
-                'Not fix android_media_AudioSystem_getMasterMute return type .'.split()
+                [100, 200, 300, 500],
+                [0, 1, 2, 3, 4]
             ],
             [
-                'Totally incorrect guess .'.split()
+                [5133, 5133]
             ]
         ]
-        dataset = {'target': {
-                   'input_ids': [
-            'Fix android_media_AudioSystem_getMasterMute return type .'.split(),
-            'Fix typo'.split()
-        ]}}
+        dataset = CommitMessageGenerationDataset(src_encodings={}, trg_encodings={'input_ids': torch.tensor([
+            [1, 2, 3, 4],
+            [1, 5, 600, 600]])})
         bleu_score = BleuChecking.BLEU_CALCULATION_EXPERIMENT.get_bleu_score(predictions, dataset)
         self.assertEqual(0.0, bleu_score)
 
     def test_only_one_example_bleu_score(self):
         predictions = [
-            [
-                'Fix android_media_AudioSystem_getMasterMute return type .'.split(),
-                'Not fix android_media_AudioSystem_getMasterMute return type .'.split()
-            ]
+            [1, 2, 3, 4],
+            [0, 1, 2, 3, 4]
         ]
-        dataset = {'target': {
-                   'input_ids': [
-            'Fix android_media_AudioSystem_getMasterMute return type .'.split()
-        ]}}
+        dataset = CommitMessageGenerationDataset(src_encodings={}, trg_encodings={'input_ids': torch.tensor([
+            [1, 2, 3, 4]])})
         bleu_score = BleuChecking.BLEU_CALCULATION_EXPERIMENT.get_bleu_score(predictions, dataset)
         self.assertEqual(100.0, bleu_score)
 
@@ -119,11 +101,9 @@ class BleuChecking(unittest.TestCase):
         predictions = [
             [], []
         ]
-        dataset = {'target': {
-                   'input_ids': [
-            'Fix android_media_AudioSystem_getMasterMute return type .'.split(),
-            'Fix typo'.split()
-        ]}}
+        dataset = CommitMessageGenerationDataset(src_encodings={}, trg_encodings={'input_ids': torch.tensor([
+            [1, 2, 3, 4],
+            [1, 5, 600, 600]])})
         bleu_score = BleuChecking.BLEU_CALCULATION_EXPERIMENT.get_bleu_score(predictions, dataset)
         self.assertEqual(0.0, bleu_score)
 
