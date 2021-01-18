d82f38714f9b3a5a1d551d5db41935f2cbddf0be
saridormi <saridormi123@gmail.com>
2021-01-18 12:05:07 +0300

    Align sequences properly using edit distance

--- dataset_utils/diff_preprocessor.py
+++ dataset_utils/diff_preprocessor.py
@@ -1,6 +1,7 @@
 import itertools
 import os
 from typing import Tuple, List
+from dataset_utils.edit_distance_utils import align_lists
 
 
 def tokenize_git_diff_output_string(git_diff_output: str) -> List[List[str]]:
@@ -27,55 +28,133 @@ class DiffPreprocessor:
         """
         tokens_per_line = tokenize_git_diff_output_string(git_diff_output)
 
-        prev_lines, updated_lines = [], []
+        prev_lines, updated_lines, removed, added = [], [], [], []
         was_special_keyword_modification = False
+        rename_len = None
+        another_file_name_len = None
+
         for tokens_in_line in tokens_per_line:
             if tokens_in_line[0] == 'mmm':
-                prev_lines.append(tokens_in_line[1:])
+                # name of changed file
+                # example: mmm a / telecomm / java / android / telecomm / Connection . java
+                # example (if new file was created): mmm / dev / null
+                if tokens_in_line[1] == 'a':
+                    prev_lines.append(tokens_in_line[3:])
+                    another_file_name_len = len(tokens_in_line) - 3
+                else:
+                    prev_lines.append(tokens_in_line[2:])
                 was_special_keyword_modification = True
+
             elif tokens_in_line[0] == 'ppp':
-                updated_lines.append(tokens_in_line[1:])
+                # name of changed file
+                # example: ppp b / telecomm / java / android / telecomm / Connection . java
+                # example (if file was deleted):  ppp / dev / null
+                if tokens_in_line[1] == 'b':
+                    updated_lines.append(tokens_in_line[3:])
+                    if prev_lines[-1] == ['dev', '/', 'null']:
+                        prev_lines[-1].extend(["<empty>" for _ in range(len(tokens_in_line) - 3)])
+                else:
+                    updated_lines.append(tokens_in_line[2:])
+                    cur_len = len(tokens_in_line) - 2
+                    updated_lines[-1].extend(["<empty>" for _ in range(another_file_name_len - cur_len)])
                 was_special_keyword_modification = True
+
             elif tokens_in_line[:3] == ['new', 'file', 'mode']:
+                # line in git diff when new file is created
+                # example: new file mode 100644
                 prev_lines.append(["<empty>", "<empty>"])
                 updated_lines.append(['new', 'file'])
                 was_special_keyword_modification = True
+
             elif tokens_in_line[:3] == ['deleted', 'file', 'mode']:
+                # line in git diff when file is deleted
+                # example: deleted file mode 100644
                 prev_lines.append(["<empty>", "<empty>"])
                 updated_lines.append(['deleted', 'file'])
                 was_special_keyword_modification = True
+
             elif tokens_in_line[:2] == ['rename', 'from']:
+                # line in git diff when file was renamed (old name)
+                # example: rename from src / forge / resources / worldedit . properties
                 prev_lines.append(tokens_in_line)
-                updated_lines.append(["<empty>" for token in tokens_in_line])
+                if rename_len:
+                    cur_len = len(tokens_in_line) - 2
+                    if cur_len < rename_len:
+                        prev_lines[-1].extend(["<empty>" for _ in range(rename_len - cur_len)])
+                    else:
+                        updated_lines[-1].extend(["<empty>" for _ in range(cur_len - rename_len)])
+                    rename_len = None
+                else:
+                    rename_len = len(tokens_in_line) - 2
                 was_special_keyword_modification = True
+
             elif tokens_in_line[:2] == ['rename', 'to']:
-                prev_lines.append(["<empty>" for token in tokens_in_line])
+                # line in git diff when file was renamed (new name)
+                # example: rename to src / forge / resources / defaults / worldedit . properties
                 updated_lines.append(tokens_in_line)
+                if rename_len:
+                    cur_len = len(tokens_in_line) - 2
+                    if cur_len < rename_len:
+                        updated_lines[-1].extend(["<empty>" for _ in range(rename_len - cur_len)])
+                    else:
+                        prev_lines[-1].extend(["<empty>" for _ in range(cur_len - rename_len)])
+                    rename_len = None
+                else:
+                    rename_len = len(tokens_in_line) - 2
                 was_special_keyword_modification = True
+
             elif tokens_in_line[:2] == ['old', 'mode']:
+                # line in git diff when file mode was changed
+                # example: old mode 100644
+                # 644=rw-r--r--
                 prev_lines.append(tokens_in_line)
                 was_special_keyword_modification = True
+
             elif tokens_in_line[:2] == ['new', 'mode']:
+                # line in git diff when file mode was changed
+                # example: new mode 100755
+                # 755=rwxr-xr-x
                 updated_lines.append(tokens_in_line)
                 was_special_keyword_modification = True
+
             elif tokens_in_line[0] == '-':
-                prev_lines.append(tokens_in_line[1:])
-                updated_lines.append(["<empty>" for token in tokens_in_line[1:]])
+                # lines that were removed
+                # example: - version = ' 2 . 0 . 2 '
+                removed.append(tokens_in_line)
+
             elif tokens_in_line[0] == '+':
-                prev_lines.append(["<empty>" for token in tokens_in_line[1:]])
-                updated_lines.append(tokens_in_line[1:])
+                # lines that were added
+                # example: + version = ' 2 . 0 . 3 '
+                added.append(tokens_in_line)
+
             elif tokens_in_line[0] == 'index' or tokens_in_line[:2] == ['similarity', 'index']:
+                # some special info that we are not interested in
+                # example 1: index 0000000 . . 3f26e45
+                # example 2: similarity index 100 %
                 continue
+
             else:
-                prev_lines.append(tokens_in_line)
-                updated_lines.append(tokens_in_line)
+                # all other cases
+                # case 1: line that was not changed (drop them)
+                # case 2: Binary files a / dependencies / windows / sumatra / SumatraPDF . exe and / dev / null differ
+                if tokens_in_line[:2] == ["Binary", "files"]:
+                    prev_lines.append(tokens_in_line)
+                    updated_lines.append(tokens_in_line)
+
+        # align removed and added
+        removed, added = align_lists(removed, added)
+        for prev_line, upd_line in zip(removed, added):
+            prev_lines.append(prev_line)
+            updated_lines.append(upd_line)
 
         prev = ' '.join(itertools.chain(*[line + ['\\n'] for line in prev_lines]))
         updated = ' '.join(itertools.chain(*[line + ['\\n'] for line in updated_lines]))
+
         if not was_special_keyword_modification:
             print(f'No special keyword found for diff: {git_diff_output}')
         if prev == updated:
             print(f'Prev and updated are the same for diff: {git_diff_output}')
+
         return prev + '\n', updated + '\n'
 
     @staticmethod
