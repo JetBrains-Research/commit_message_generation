1883b3d6584186bb54635b235c1e7a1d95c67d1d
saridormi <saridormi123@gmail.com>
2021-04-15 14:45:55 +0300

    Update model class to support both training from scratch and loading pretrained models (+ uniformly removing layers from pretrained encoder or decoder)

--- model/encoder_decoder_module.py
+++ model/encoder_decoder_module.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from collections import defaultdict
+from copy import copy
+from typing import Optional
 
 import wandb
 
@@ -19,12 +21,14 @@ nltk.download('wordnet')
 class EncoderDecoderModule(pl.LightningModule):
     def __init__(self,
                  learning_rate: float,
-                 num_layers_encoder: int,
-                 num_layers_decoder: int,
                  src_tokenizer: RobertaTokenizer,
                  trg_tokenizer: GPT2Tokenizer,
                  num_epochs: int,
                  num_batches: int,
+                 num_layers_encoder: Optional[int] = None,
+                 num_layers_decoder: Optional[int] = None,
+                 encoder_name_or_path: Optional[str] = None,
+                 decoder_name_or_path: Optional[str] = None,
                  **kwargs):
         super().__init__()
 
@@ -36,20 +40,41 @@ class EncoderDecoderModule(pl.LightningModule):
 
         self.save_hyperparameters()
 
-        # use randomly initialized RoBERTa as encoder
-        encoder_config = RobertaConfig()
-        encoder_config.num_hidden_layers = num_layers_encoder
-        encoder = RobertaModel(config=encoder_config)
+        if encoder_name_or_path is not None and decoder_name_or_path is not None:
+            # use pretrained RoBERTa as encoder
+            encoder = RobertaModel.from_pretrained(encoder_name_or_path)
+            # resize embeddings to match vocabulary size
+            encoder.resize_token_embeddings(len(self._src_tokenizer))
+            # remove layers if necessary
+            if num_layers_encoder is not None and num_layers_encoder < encoder.config.num_hidden_layers:
+                encoder = EncoderDecoderModule.remove_layers_from_model(encoder, num_layers_encoder, is_gpt=False)
 
-        # resize embeddings to match CodeBERT vocab
-        encoder.resize_token_embeddings(len(self._src_tokenizer))
+            # use pretrained GPT-2 as decoder
+            config = GPT2Config.from_pretrained(decoder_name_or_path)
+            config.is_decoder = True
+            config.add_cross_attention = True
+            decoder = GPT2LMHeadModel.from_pretrained(decoder_name_or_path, config=config)
+            # remove layers if necessary
+            if num_layers_decoder is not None and num_layers_decoder < decoder.config.n_layer:
+                decoder = EncoderDecoderModule.remove_layers_from_model(decoder, num_layers_decoder, is_gpt=True)
 
-        # use randomly initialized GPT-2 as decoder
-        decoder_config = GPT2Config()
-        decoder_config.n_layer = num_layers_decoder
-        decoder_config.is_decoder = True
-        decoder_config.add_cross_attention = True
-        decoder = GPT2LMHeadModel(config=decoder_config)
+        elif num_layers_decoder is not None and num_layers_encoder is not None:
+            # use randomly initialized RoBERTa as encoder
+            encoder_config = RobertaConfig()
+            encoder_config.num_hidden_layers = num_layers_encoder
+            encoder = RobertaModel(config=encoder_config)
+            # resize embeddings to match vocabulary size
+            encoder.resize_token_embeddings(len(self._src_tokenizer))
+
+            # use randomly initialized GPT-2 as decoder
+            decoder_config = GPT2Config()
+            decoder_config.n_layer = num_layers_decoder
+            decoder_config.is_decoder = True
+            decoder_config.add_cross_attention = True
+            decoder = GPT2LMHeadModel(config=decoder_config)
+        else:
+            raise ValueError("You have to specify either num_layers for training from scratch \
+                                          or paths for loading pretrained models")
 
         self.model = EncoderDecoderModel(encoder=encoder, decoder=decoder)
 
@@ -249,3 +274,47 @@ class EncoderDecoderModule(pl.LightningModule):
                      'interval': 'step',
                      'frequency': 1}
         return [optimizer], [scheduler]
+
+    @staticmethod
+    def remove_layers_from_model(teacher, num_layers, is_gpt):
+        if not is_gpt:
+            teacher_config = teacher.config
+            student_config = copy(teacher.config)
+            student_config.num_hidden_layers = num_layers
+            student = RobertaModel(config=student_config)
+
+            # copy all embeddings
+            student.embeddings.word_embeddings = teacher.embeddings.word_embeddings
+            student.embeddings.position_embeddings = teacher.embeddings.position_embeddings
+            student.embeddings.token_type_embeddings = teacher.embeddings.token_type_embeddings
+            student.embeddings.LayerNorm = teacher.embeddings.LayerNorm
+            student.embeddings.dropout = teacher.embeddings.dropout
+
+            # uniformly pick from middle layers from teacher
+            # it is basically np.linspace(0, teacher_config.num_hidden_layers,
+            #                             num=student_config.num_hidden_layers, endpoint=True)
+            step = (teacher_config.num_hidden_layers - 1) / (student_config.num_hidden_layers - 1)
+            for student_layer, teacher_layer in enumerate(
+                    int(i * step) for i in range(student_config.num_hidden_layers)):
+                student.encoder.layer[student_layer] = teacher.encoder.layer[teacher_layer]
+
+        else:
+            teacher_config = teacher.config
+            student_config = copy(teacher.config)
+            student_config.n_layer = num_layers
+
+            student = GPT2LMHeadModel(config=student_config)
+
+            # Copying all embeddings
+            student.transformer.wte = teacher.transformer.wte
+            student.transformer.wpe = teacher.transformer.wpe
+            student.transformer.drop = teacher.transformer.drop
+            # Maybe there is something else in BERT that need to be copied!
+            # Specific thing for GPT2LMHead. Not necessary for BERT
+            student.tie_weights()
+            # Uniformly pick from middle layers from teacher
+            # It is basically np.linspace(0, teacher_config.n_layer, num=student_config.n_layer, endpoint=True)
+            step = (teacher_config.n_layer - 1) / (student_config.n_layer - 1)
+            for student_layer, teacher_layer in enumerate(int(i * step) for i in range(student_config.n_layer)):
+                student.transformer.h[student_layer] = teacher.transformer.h[teacher_layer]
+        return student
