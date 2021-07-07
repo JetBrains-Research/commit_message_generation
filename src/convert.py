a9b2445fd9fef3d7fafd046b542dc7cec619ad3c
saridormi <saridormi123@gmail.com>
2021-07-07 15:21:56 +0300

    Second try to fix mypy errors

--- src/convert.py
+++ src/convert.py
@@ -2,7 +2,7 @@ import hydra
 import os
 from omegaconf import DictConfig
 from onnx_utils import ONNXConverter  # type: ignore
-from onnx_utils import DataCreator    # type: ignore
+from onnx_utils import DataCreator  # type: ignore
 
 
 @hydra.main(config_path="..", config_name="config")
